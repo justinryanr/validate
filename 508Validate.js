a2b1cf4225c20ac03321906d1b65e1d496ae357c
justinryanr <justin.rings1@gmail.com>
2013-02-27 12:37:02 -0800

    Update 508Validate.js

--- 508Validate.js
+++ 508Validate.js
@@ -5,7 +5,7 @@
  * A simple Form Validating Library
  * 
  * It uses the aria-invalid attribute to validate for 508 complaince 
- * You can set your own CSS to make the fields pop out to the user
+ * YOu can set your own CSS to make the fields pop out to the user
  * 
  * use it: 
  *   check.text('id') - Validates that there is text in the field...its pretty forgiving
@@ -14,70 +14,87 @@
  * 
  */
 
-
-var check = function(){
+(function(){
 	
 	var emailPat = /^(([-\w\d]+)(\.[-\w\d]+)*@([-\w\d]+)(\.[-\w\d]+)*(\.([a-zA-Z]{2,5}|[\d]{1,3})){1,2})$/;
 	var urlPat = /^(http[s]?:\/\/)?([-\w\d]+)(\.[-\w\d]+)*(\.([a-zA-Z]{2,5}|[\d]{1,3})){1,2}(\/([-~%\.\(\)\w\d]*\/*)*(#[-\w\d]+)?)?$/;
+	var datePat = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
 	var val;
 	
-	
-  return{
-    text:  function(id){
-    	        this.elem = document.getElementById(id);
-		this.elem.addEventListener('change',function(){valText(this)},false);
-           },
-          
-    email: function(id){
-    	        this.elem = document.getElementById(id);
-		this.elem.addEventListener('change',function(){valEmail(this)},false);      
-           },
-             
-    url:   function(id){
-    	        this.elem = document.getElementById(id);
-		this.elem.addEventListener('change',function(){valUrl(this)},false);
-           }
-  }
-
-	function isBlank(e){
-	  val = e.value
-	  if(val == null || val.length == 0)
-	  return /\S/.test(e);
-	  return false;
-	}
-	
-	function valText (field){
-	  if(isBlank(field)){
-	    field.setAttribute('aria-invalid', 'true');
-	  }else{
-	    field.setAttribute('aria-invalid', 'false');
+	window.check = {
+	    text:  function(id){
+	    	        this.elem = document.getElementById(id);
+				   	this.elem.addEventListener('change',function(){valText(this)},false);
+	           },
+	          
+	    email: function(id){
+	    	        this.elem = document.getElementById(id);
+				   	this.elem.addEventListener('change',function(){valEmail(this)},false);      
+	           },
+	             
+	    url:   function(id){
+	    	        this.elem = document.getElementById(id);
+			   	    this.elem.addEventListener('change',function(){valUrl(this)},false);
+	          },
+	    date:  function(id){
+	    	        this.elem = document.getElementById(id);
+			   	    this.elem.addEventListener('change',function(){valDate(this)},false);
+	           }           
 	  }
-	}
 	
-	function valEmail(field){
-	  if(!isBlank(field))
-	  {
-	    if(emailPat.test(field.value)){
-	      field.setAttribute('aria-invalid', 'false');
-	    }else{
-	      field.setAttribute('aria-invalid', 'true');
-	    }
-	  }else{
-	    alert("Field is Blank");
-	  }
-	}
+		function isBlank(e){
+			    val = e.value
+				if(val == null || val.length == 0)
+				return /\S/.test(e);
+				return false;
+		}
+		
+		function valText (field){
+		    if(isBlank(field)){
+				field.setAttribute('aria-invalid', 'true');
+			}else{
+				field.setAttribute('aria-invalid', 'false');
+			}
+		}
+		
+		function valEmail(field){
+			if(!isBlank(field))
+			{
+				if(emailPat.test(field.value)){
+					field.setAttribute('aria-invalid', 'false');
+				}else{
+					field.setAttribute('aria-invalid', 'true');
+				}
+			}else{
+				field.setAttribute('aria-invalid', 'true');
+			}
+		}
+		
+		function valUrl(field){	
+			if(!isBlank(field))
+			{
+				if(urlPat.test(field.value)){
+					field.setAttribute('aria-invalid', 'false');
+				}else{
+					field.setAttribute('aria-invalid', 'true');
+				}
+			}else{
+				field.setAttribute('aria-invalid', 'true');
+			}
+		}
+		
+		function valDate(field){	
+			if(!isBlank(field))
+			{
+				if(datePat.test(field.value)){
+					field.setAttribute('aria-invalid', 'false');
+				}else{
+					field.setAttribute('aria-invalid', 'true');
+				}
+			}else{
+				field.setAttribute('aria-invalid', 'true');
+			}
+		}
 	
-	function valUrl(field){	
-	  if(!isBlank(field))
-	  {
-	    if(urlPat.test(field.value)){
-	      field.setAttribute('aria-invalid', 'false');
-	    }else{
-	      field.setAttribute('aria-invalid', 'true');
-	    }
-          }else{
-	    field.setAttribute('aria-invalid', 'true');
-          }
 	}
-
-}();
+)();
